21f31fea0dcadaeb5a3f5b690874f6a360b6fec6
Rona ¯\_(ツ)_/¯ <deasnataly@gmail.com>
2018-06-25 21:20:28 +0700

    First working query! Hooray
    '

---
+++ client/components/SongList.js
@@ -0,0 +1,35 @@
+import gql from 'graphql-tag';
+import React, { Component } from 'react';
+import { graphql } from 'react-apollo';
+
+class SongList extends Component {
+  renderSongs() {
+    return this.props.data.songs.map(song =>
+        (
+            <li key={song.id} className="collection-item">
+              {song.title}
+            </li>
+        )
+    );
+  }
+
+  render() {
+    if (this.props.data.loading) return (<div> Loading...</div>);
+    return (
+        <ul className="collection">
+          {this.renderSongs()}
+        </ul>
+    );
+  }
+}
+
+const query = gql`
+  {
+    songs {
+      id
+      title
+    }
+  }
+`;
+
+export default graphql(query)(SongList);
