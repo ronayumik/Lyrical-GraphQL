4cba8bcb3216cf39afce7b8777299eb85cde359e
Rona ¯\_(ツ)_/¯ <deasnataly@gmail.com>
2018-06-25 21:31:33 +0700

    Adding routing for preparation to Mutation: Add Song

--- client/index.js
+++ client/index.js
@@ -1,7 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
 import ApolloClient from 'apollo-client';
+import React from 'react';
 import { ApolloProvider } from 'react-apollo';
+import ReactDOM from 'react-dom';
+import { hashHistory, IndexRoute, Route, Router } from 'react-router';
+import App from './components/App';
 import SongList from './components/SongList';
 
 const client = new ApolloClient({});
@@ -9,12 +11,16 @@ const client = new ApolloClient({});
 const Root = () => {
   return (
       <ApolloProvider client={client}>
-        <SongList />
+        <Router history={hashHistory}>
+          <Route path="/" component={App}>
+            <IndexRoute component={SongList} />
+          </Route>
+        </Router>
       </ApolloProvider>
-  )
+  );
 };
 
 ReactDOM.render(
-  <Root />,
-  document.querySelector('#root')
+    <Root />,
+    document.querySelector('#root')
 );
